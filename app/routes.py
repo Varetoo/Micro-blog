1fecff21f4a36b89f22a85fb99a02cb67903d19b
Varetoo <alvaromi2005@gmail.com>
2025-02-03 22:12:03 +0100

    Agregar funcionalidad de renderizado de plantillas y crear páginas de inicio y base para la aplicación Flask

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {"username": "Alvaro"}
+    posts = [
+        {
+            "author": {"username":"Paco"},
+            "body": "Bonito día el de hoy!!"
+        },
+        {
+            "author": {"username":"Pepe"},
+            "body": "Me siento genial!!"
+        },
+        {
+            "author": {"username":"Juan"},
+            "body": "Estoy muy cansado!!"
+        }
+    ]
+    return render_template("index.html", title="Home page", user=user, posts=posts)
