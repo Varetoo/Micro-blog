19cdd2eedbc3070fce304c7d419a41c0859f6088
Varetoo <alvaromi2005@gmail.com>
2025-02-05 00:06:34 +0100

    Actualizar nombres de usuario y contenido de publicaciones en la página de inicio

--- app/routes.py
+++ app/routes.py
@@ -6,15 +6,19 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Alvaro'}
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'author': {'username': 'Paco'},
+            'body': 'Un dia precioso en Madrid!'
         },
         {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'author': {'username': 'Pepe'},
+            'body': 'El avion se retraso'
+        },
+        {
+            'author': {'username': 'Juan'},
+            'body': 'Estoy aprendiendo Flask'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
